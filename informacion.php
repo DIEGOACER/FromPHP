28afca17e4d4a49a5a19b00346d1d097f31a8620
DIEGOACER <d79959925@gmail.com>
2024-10-25 14:46:05 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion= $_POST ['Identificacion];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST [ 'Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del empleado es: ".Identificaion;
+echo "Los nombres y apellidos del empleado son: ".NomApllidos;
+echo " El cargo del empleado es: ".Cargo,
+echo "El salario del empleado es: "Salario;
+  ?>
